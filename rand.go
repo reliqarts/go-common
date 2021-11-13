cce209f3a8f77ec41ab5e96046c625dafa8a3d71
reliq <reliq@reliqarts.com>
2021-11-13 05:46:12 +0100

    + random string generation

---
+++ rand.go
@@ -0,0 +1,26 @@
+package common
+
+import (
+	"crypto/rand"
+	mRand "math/rand"
+)
+
+// RandomBytes generates a slice of [length] random bytes.
+func RandomBytes(n int) ([]byte, error) {
+	key := make([]byte, n)
+	_, err := rand.Read(key)
+
+	return key, err
+}
+
+// RandomString generates a random string of length n.
+func RandomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[mRand.Intn(len(letters))]
+	}
+
+	return string(s)
+}
